475acefdca42b651bbf274daf013f596e0989926
LuisaLafaurie <m1m1respald0w0@gmail.com>
2023-04-10 10:18:02 -0500

    prueba de funcionalidad de botones e impresión de datos

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 const URL_API = "https://pokeapi.co/api/v2/pokemon";
-const sectionPokemons = document.querySelector(".section__pokemon");
-const sectionInfoPokemon = document.querySelector(".section__information");
-const sectionOthersPokemons = document.querySelector(".footer");
+const pokemonDetailsCover = document.querySelector(".section__pokemon");
+const pokemonDetailsBoard = document.querySelector(".section__information");
+const pokemonButtons = document.querySelector(".footer");
 
-//Funcion que me retorna un número entero aleatorio entre min(incluido) y max(excluido).
-const getRandomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+//Obtener detalles de los pokemons e imprimirlos
 
-const getPokemons = async (id) => {
+const getPokemons = async (name) => {
   try {
-    const { data } = await axios.get(`${URL_API}/${id}`); //desestructuración de objetos
-    console.log(data);
+    const { data } = await axios.get(`${URL_API}/${name}`); //desestructuración de objetos
+    // console.log(data);
+    // printDetailsBoard(data)
+    // printDetailsCover(data)
+    // printButtonsPokemons(data)
     return data;
   } catch (error) {
     console.log(error);
   }
 };
 
-const getAllInfoPokemon = async (id) => {
-//   const allInfoPokemon = [];
+async function getAllInfoPokemon(name) {
+  //   const allInfoPokemon = [];
   try {
-    const { data } = await axios.get(`${URL_API}/${id}`); //desestructuración de objetos
+    const { data } = await axios.get(`${URL_API}/${name}`); //desestructuración de objetos
 
     const poke = {
       id: data.id,
@@ -32,6 +32,9 @@ const getAllInfoPokemon = async (id) => {
       abilities: data.abilities[0].ability.name,
       type: data.types[0].type.name
     };
+
+    // pokemonDetailsCover(poke)
+    // pokemonDetailsBoard(poke)
     // allInfoPokemon.push(poke);
     return poke;
   } catch (error) {
@@ -39,54 +42,117 @@ const getAllInfoPokemon = async (id) => {
   }
 };
 
-const printCardPokemon = (poke, container) => {
-  console.log(poke);
-  container.innerHTML = `
-        <div class="section__pokemon-name">
-            <h2 class="section__pokemon-title">${poke.name}</h2>
+async function getAllPokemonBotons(name) {
+  //   const allInfoPokemon = [];
+  try {
+    const { data } = await axios.get(`${URL_API}/${name}`); //desestructuración de objetos
+
+    const poke = {
+      id: data.id,
+      name: data.name,
+      image: data.sprites.other.dream_world.front_default,
+    };
+
+    // pokemonDetailsCover(poke)
+    // pokemonDetailsBoard(poke)
+    // allInfoPokemon.push(poke);
+    return poke;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Pintar los botones de los pokemones
+
+function printDetailsCover(pokemon) {
+  pokemonDetailsCover.innerHTML = `
+  <div class="section__pokemon-name">
+    <figure class="section__pokemon-icon">
+      <img
+      src="${pokemon.sprites.front_default}"
+      alt="Icono Fuego"/>
+    </figure>
+    <h2 class="section__pokemon-title">${pokemon.name}</h2>
           </div>
           <figure class="section__img">
             <img
-              src="${poke.image}"
-              alt=${poke.name}
-            />
-          </figure>
-        </div>
-    `;
+              src="${pokemon.sprites.other.dream_world.front_default}"
+              alt="${pokemon.name}"/>
+   </figure>
+  `;
+}
+
+// printDetailsCover(URL_API, getAllInfoPokemon);
+
+function printDetailsBoard(pokemon) {
+  pokemonDetailsBoard.innerHTML = `
+  <span>NO.<p>${pokemon.id}</p></span>
+  <span>LEVEL<p>100</p></span>
+  <span>TYPE<p>${pokemon.type}</p></span>
+  <span>HABILITY<p>${pokemon.abilities}</p></span>
+  <span>HEIGHT<p>${pokemon.height}</p></span>
+  <span>WEIGHT<p>${pokemon.weight}</p></span>
+  `;
 };
 
-const printInfoPokemon = (poke, container) => {
-  container.innerHTML = `
-    <span>NO.<p>${poke.id}</p></span>
-    <span>LEVEL<p>100</p></span>
-    <span>TYPE<p>${poke.type}</p></span>
-    <span>HABILITY<p>${poke.abilities}</p></span>
-    <span>HEIGHT<p>${poke.height}</p></span>
-    <span>WEIGHT<p>${poke.weight}</p></span>
-    `;
+
+// printDetailsBoard(URL_API, getAllInfoPokemon);
+
+function printButtonsPokemons(pokemon) {
+  pokemonButtons.innerHTML += `
+<button class="pokeBoton" data-url="${URL_API}">
+<img src=${pokemon.image} />
+${pokemon.name}
+</button>
+`;
+}
+
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
-// const printOtherPokemons = (poke, container) => {
-//     container.innerHTML =`
-//     <figure class="footer_figure">
-//     <img
-//       src="${poke.sprites.other.dream_world.front_default}"
-//       alt="${poke.name}"
-//     />
-//     </figure>
-
-//     `;
-// };
-
+// Imprimir la portada y la tabla de información de forma aleatoria
 document.addEventListener("DOMContentLoaded", async () => {
-  const random = getRandomInt(1, 151);
-  const pokemon = await getPokemons(random);
-  console.log(pokemon);
-  
-  const allInfo = await getAllInfoPokemon(random);
-  console.log(allInfo);
-  
-  printInfoPokemon(allInfo, sectionInfoPokemon);
-  printCardPokemon(allInfo, sectionPokemons);
-  // printOtherPokemons(pokemon, sectionOthersPokemons);
+  let random = getRandomInt(1, 100);
+
+  let pokemonRandomCover = await getPokemons(random);
+  let pokemonRandomBoard = await getAllInfoPokemon(random);
+
+  printDetailsCover(pokemonRandomCover, pokemonDetailsCover)
+  printDetailsBoard(pokemonRandomBoard, pokemonDetailsBoard)
+
 });
+
+
+//Ciclo para generar los botones
+
+for (let i = 0; i < 4; i++) {
+  const randomId = Math.floor(Math.random() * 200) + 1;
+
+  let pokemonRandomButton = await getAllPokemonBotons(randomId);
+  printButtonsPokemons(pokemonRandomButton, pokemonButtons);
+}
+//--------------------------------------------------------------------
+
+
+// Imprimir la portada y la tabla de información al hacerle click a un botón
+
+// let buttonPoke = document.getElementsByClassName("pokeBoton");
+// buttonPoke = addEventListener("click", async () => {
+//   console.log("hice click");
+// }) //Solo me escucha el click
+
+// document.addEventListener("DOMContentLoaded", async (e) => {
+//   let urlPokemon = e.target.getAttribute("data-url");
+
+//   // if(urlPokemon){
+//   //   console.log("hola")
+//   //   const poke = await getPokemons(urlPokemon);
+//   //   printDetailsCover(poke, pokemonDetailsCover)
+//   // }
+
+//   // for (let i = 0; i < 0; i++){
+//   //   const poke = await getPokemons(urlPokemon);
+//   //   printDetailsCover(poke, pokemonDetailsCover)
+//   // }
+// });
